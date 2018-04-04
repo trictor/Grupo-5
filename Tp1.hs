50cf49ea519516c7c4052a27ce4177d8c3b07733
Marcos Donner <marcos.donner.97@gmail.com>
2018-04-04 19:28:08 -0300

    Terminado todos los eventos
    Termine todos los eventos y los estuve probando con sus limites y demas asi que digamos que la primera parte ya esta lista.

--- Tp1.hs
+++ Tp1.hs
@@ -5,7 +5,7 @@ import Data.Maybe -- por si llegan a usar un metodo de coleccion y devuelva Noth
 -- import Test.Hspec -- para usar los test
 
 
-mostrarBilletera usuario = usuario
+--Basicamente termine las funciones de cada evento
 
 sumarDinero :: Float -> Usuario -> Usuario
 sumarDinero unDinero unUsuario = unUsuario {
@@ -17,30 +17,36 @@ restarDinero unDinero unUsuario = unUsuario{
              billetera = billetera unUsuario - unDinero
 }
 
+hacerAumento unUsuario = unUsuario {
+             billetera = billetera unUsuario + billetera unUsuario * 1.20 }
+
+esMayorADiez unUsuario = (billetera.hacerAumento) unUsuario >= (billetera.sumarDinero 10) unUsuario
+
 deposito :: Float -> Usuario -> Usuario
-deposito dineroDepositado unUsuario = sumarDinero dineroDepositado unUsuario
+deposito dineroDepositado = sumarDinero dineroDepositado
 
 extraccion :: Float -> Usuario -> Usuario
-extraccion dineroExtraido unUsuario | (billetera.restarDinero dineroExtraido) unUsuario <=0  = unUsuario{billetera = 0}
-                                    | otherwise = (restarDinero dineroExtraido unUsuario)
+extraccion dineroExtraido unUsuario | (billetera.restarDinero dineroExtraido) unUsuario  <=0  = unUsuario{billetera = 0}
+                                    | otherwise = restarDinero dineroExtraido unUsuario
 
+upgrade unUsuario | esMayorADiez unUsuario = sumarDinero 10 unUsuario
+                  | otherwise = hacerAumento unUsuario
 
-                    --falta terminar para el tope de 10 unidades
---upgrade unaBilletera  = (+) (aumentoBilletera unaBilletera) unaBilletera
---aumentoBilletera laBilletera = laBilletera * 0.2
---cierreDeCuenta unaBilletera = 0
---quedaIgual unaBilletera = unaBilletera
+cierreDeCuenta unUsuario = unUsuario {
+               billetera = 0
+}
 
-          -- usuarios
+quedaIgual unUsuario = unUsuario
+
 data Usuario = Usuario {
-                  nombre :: String,
-                  billetera :: Float
-                  }deriving (Show, Eq)
+        nombre :: String,
+        billetera :: Float
+        }deriving (Show, Eq)
 
 pepe = Usuario "pepe" 10
 lucho = Usuario "lucho" 2
 
-        -- transacciones
+-- transacciones
 -- transacciones unUsuario tipoTransaccion = unUsuario (billetera (tipoTransaccion ) )
 
 pepe2 = Usuario "pepe2" 20
